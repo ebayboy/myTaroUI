8974511b60377da5b7d6f59203e3c24e632815fb
fanpf <ebayboy@aliyun.com>
2020-01-30 18:17:43 +0800

    attabas

--- src/atcomponent/navigation/atTabsTest.jsx
+++ src/atcomponent/navigation/atTabsTest.jsx
@@ -1,7 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 
-import { AtTabs } from 'taro-ui'
+import { AtTabs, AtTabsPane } from 'taro-ui'
 import 'taro-ui/dist/style/index.scss'
 
 export default class AtTabsTest extends Component {
@@ -22,6 +22,15 @@ export default class AtTabsTest extends Component {
                     tabList={[{ title: 'tab1' }, { title: 'tab2' }, { title: 'tab3' }]}
                     onClick={this.handleClick.bind(this)}
                 >
+                    <AtTabsPane current={this.state.current} index={0} >
+                        <View style='padding: 100px 50px;background-color: #FAFBFC;text-align: center;' >标签页一的内容</View>
+                    </AtTabsPane>
+                    <AtTabsPane current={this.state.current} index={1}>
+                        <View style='padding: 100px 50px;background-color: #FAFBFC;text-align: center;'>标签页二的内容</View>
+                    </AtTabsPane>
+                    <AtTabsPane current={this.state.current} index={2}>
+                        <View style='padding: 100px 50px;background-color: #FAFBFC;text-align: center;'>标签页三的内容</View>
+                    </AtTabsPane>
                 </AtTabs>
             </View>
         )
