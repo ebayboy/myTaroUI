13109ac31ecc3ea645ef9e893e8a80c80745ef66
fanpf <ebayboy@aliyun.com>
2020-01-30 22:13:39 +0800

    fw

--- src/atcomponent/navigation/atIndexesTest.jsx
+++ src/atcomponent/navigation/atIndexesTest.jsx
@@ -1,8 +1,11 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 
-import { AtIndexes } from 'taro-ui'
+import { AtSearchBar, AtIndexes } from 'taro-ui'
 import 'taro-ui/dist/style/index.scss'
+
+import mockData, { CityItem } from './mock-data'
+import './atIndexesTest.scss'
 
 export default class AtIndexesTest extends Component {
     constructor() {
@@ -14,39 +17,42 @@ export default class AtIndexesTest extends Component {
 
     render() {
 
-    const list = [{
-        title: 'A',
-        key: 'A',
-        items: [
-            {
-                'name': '阿坝'
-                // 此处可加其他业务字段
-            },
-            {
-                'name': '阿拉善'
-            }]
-    },
-    {
-        title: 'B',
-        key: 'B',
-        items: [
-            {
-                'name': '北京'
-            },
-            {
-                'name': '保定'
-            }]
-    }
-    ]
+        const list = [{
+            title: 'A',
+            key: 'A',
+            items: [
+                {
+                    'name': '阿坝'
+                    // 此处可加其他业务字段
+                },
+                {
+                    'name': '阿拉善'
+                }]
+        },
+        {
+            title: 'B',
+            key: 'B',
+            items: [
+                {
+                    'name': '北京'
+                },
+                {
+                    'name': '保定'
+                }]
+        }
+        ]
 
 
         return (
             <View style='height:100vh'>
                 <AtIndexes
-                    list={list}
+                    topKey='Top'
+                    list={mockData}
                     onClick={this.onClick.bind(this)}
                 >
-                    <View>自定义内容</View>
+                    <View className='custom-area'>
+                        用户自定义内容
+                    </View>
                 </AtIndexes>
             </View>
         )
